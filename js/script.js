6ea47fdf292cdf14ecb2b29ee3664e076ce3d9e3
Amit Sarker <amitsarker255@gmail.com>
2022-08-22 15:40:38 +0600

    total team cost added

--- js/script.js
+++ js/script.js
@@ -38,13 +38,31 @@ function getInputFieldValueById(inputFieldId) {
   inputField.value = "";
   return inputFieldValue;
 }
+function getTextElementValueById(elementId) {
+  const element = document.getElementById(elementId);
+  const elementValueString = element.innerText;
+  const elementValue = parseFloat(elementValueString);
+  return elementValue;
+}
 function setTextElementValueById(elementId, newValue) {
   const textElement = document.getElementById(elementId);
   textElement.innerText = newValue;
 }
+
 document.getElementById("playerCost").addEventListener("click", function () {
   const perPlayerCost = getInputFieldValueById("totalPlayer");
   const totalSelectedPlayer = playerArray.length;
   const totalSelectedPlayerCost = perPlayerCost * totalSelectedPlayer;
   setTextElementValueById("totalPlayerCost", totalSelectedPlayerCost);
+});
+
+//total team cost
+
+document.getElementById("teamCost").addEventListener("click", function () {
+  const managerCost = getInputFieldValueById("managerCost");
+  const coachCost = getInputFieldValueById("coachCost");
+  const previousPlayerCost = getTextElementValueById("totalPlayerCost");
+  console.log(previousPlayerCost);
+  const totalTeamCost = previousPlayerCost + managerCost + coachCost;
+  setTextElementValueById("totalTeamCost", totalTeamCost);
 });
